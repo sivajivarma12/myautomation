2cc3fa0beb38bcd6e2d9c29cd849f4bee480393a
sivajivarma12 <sivajivarma12@gmail.com>
2025-04-08 23:32:13 +0530

    Assignment1

---
+++ src/com/turotrial/sample/DataTypeCheck.java
@@ -0,0 +1,11 @@
+package com.turotrial.sample;
+
+public class DataTypeCheck {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int a=0;
+       System.out.println(a);
+	}
+
+}
---
+++ src/com/tutorial/Assignments/Assignment1.java
@@ -0,0 +1,92 @@
+package com.tutorial.Assignments;
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Temperature of a city in degrees Celsius: 25.5
+		float celsius = 25.5f;
+		System.out.println("Temperature of a city in degrees Celsius:"+celsius);
+		
+		//Whether a customer has placed an order: true or false
+        boolean customerOrderStatus = false;
+        System.out.println("Whether a customer has placed an order:"+customerOrderStatus);
+        
+        //Person's phone number: "123-456-7890"
+		String phoneNumber = "123-456-7890";
+		System.out.println("Person's phone number:"+phoneNumber);
+		
+		//Amount of money in a customer's bank account: 1000.50
+		float money = 1000.50f;
+		System.out.println("Amount of money in a customer's bank account:"+money);
+		
+		//Person's email address: "john.doe@example.com"
+		String emailAddress = "john.doe@example.com";
+		System.out.println("Person's email address:"+emailAddress);
+		
+		//Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+		float latitude = 37.7749f;
+		double longitude= -122.4194d;
+		System.out.println("Coordinates of a location (latitude, longitude):"+latitude+","+longitude);
+		
+		//Person's marital status: true or false
+		boolean maritalStatus = true;
+		System.out.println("Person's marital status:"+maritalStatus);
+		
+		//Person's occupation: "Software Engineer"
+		String occupation = "Software Engineer";
+		System.out.println("Person's occupation:"+occupation);
+		
+		//Person's favourite colour: "Blue"
+		String colour="Blue";
+		System.out.println("Person's favourite colour:"+colour);
+		
+		
+		//Current year: 2023
+		int year= 2023;
+		System.out.println("Current year: "+year);
+				
+		//Number of followers on a social media platform: 1,000,000
+		int followersCount=1000000;
+		System.out.println("Number of followers on social media paltform:"+followersCount);
+				
+		//Rating of a movie: 7.5
+		float movieRating = 7.5f;
+		System.out.println("Rating of a movie:"+movieRating);
+		
+		//Person's blood type: 'A'
+		char bloodType = 'A';
+		System.out.println("Person's blood type:"+bloodType);
+		
+		//Title of a book: "To Kill a Mockingbird"
+		String bookTitle = "To Kill a Mockingbird";
+		System.out.println("Title of a book: "+bookTitle);
+		
+		//Number of employees in a company: 500
+		int empCount=500;
+		System.out.println("Number of employees in a company:"+empCount);
+		
+		//Time of an event: 2:30 PM
+		String time="2:30 PM";
+		System.out.println("Time of an event:"+time);
+		
+		//Name of a country: "United States"
+		String country = "United States";
+		System.out.println("Name of a country:"+country);
+		
+		//Person's eye color: "Brown"
+		String eyeColor="Brown";
+		System.out.println("Person's eye color:"+eyeColor);
+	   
+		//Person's birthplace: "New York City"
+		String birthPlace = "New York City";
+		System.out.println("Person's birthplace:"+birthPlace);
+		
+		//Distance between two cities: 200.5
+	    float distance=200.5f;
+	    System.out.println("Distance between two cities:"+distance);
+	    
+	}
+
+}
