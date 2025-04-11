fb15b803f5f028cbec28eae5f4995315e43b45ac
sivajivarma12 <sivajivarma12@gmail.com>
2025-04-11 20:17:58 +0530

    Assignment after suggestions

--- src/com/tutorial/Assignments/EmployeeGroup.java
+++ src/com/tutorial/Assignments/EmployeeGroup.java
@@ -6,11 +6,10 @@ public class EmployeeGroup extends Employee{
 	public static void main(String[] args) {
 		
 		EmployeeGroup emp = new EmployeeGroup();
-	String[] employee = emp.EmployeesName;
-	int[]  emplyeeId = emp.EmployeeID;
+	
   
-	System.out.println("Employee Name "+employee[0]);
-	System.out.println("Employee ID "+emplyeeId[0]);
+	System.out.println("Employee Name "+emp.EmployeesName[0]);
+	System.out.println("Employee ID "+emp.EmployeeID[0]);
 	}
 
 }
