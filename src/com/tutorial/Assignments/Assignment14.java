034bc303617a13b453d5cd1d2dae28040ec203cd
sivajivarma12 <sivajivarma12@gmail.com>
2025-05-05 23:06:39 +0530

    assignment14

--- src/com/tutorial/Assignments/Assignment14.java
+++ src/com/tutorial/Assignments/Assignment14.java
@@ -4,27 +4,27 @@ public class Assignment14 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int number=6;
+		int number=5;
        int value;
 		
-		for(int i=number;i>=0;i--)
+		for(int i=number;i>0;i--)
 		{
-			value=6;
-			for (int j=i; j>=1 ;j--)
+			value=5;
+			for (int j=i; j>1 ;j--)
 			{
 				System.out.print(" ");
 				--value;
 			}
 			for(int k=1;k<=value;k++)
 			{
-				System.out.print(" "+k);
+				System.out.print(k+" ");
 			}
 			System.out.println();
 		}
 		
 		for(int i=1;i<number;i++)
 		{
-			value=6;
+			value=5;
 			for(int space=i;space>=1;space--)
 			{	
 				System.out.print(" ");
@@ -32,7 +32,7 @@ public class Assignment14 {
 			}
 			for(int k=1;k<=value;k++)
 			{
-				System.out.print(" "+k);
+				System.out.print(k+" ");
 			}
 			System.out.println();
 		}
